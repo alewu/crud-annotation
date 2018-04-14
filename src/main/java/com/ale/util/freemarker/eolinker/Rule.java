fdb2b07d851341cce1163a7c1381db88e3e24d2a
alewu <wywujw@163.com>
2018-04-14 18:10:03 +0800

    update

---
+++ src/main/java/com/ale/util/freemarker/eolinker/Rule.java
@@ -0,0 +1,17 @@
+package com.ale.util.freemarker.eolinker;
+
+import lombok.Data;
+
+@Data
+public class Rule {
+    /**
+     * paramKey :
+     * paramType : 0
+     * $index : 0
+     */
+
+    private String paramKey;
+    private String paramType;
+    private int $index;
+
+}
